1073c2b83c30d3853169d55e49562f38cf8d1608
Artur <artroyal.pro@gmail.com>
2020-09-08 20:44:27 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.7a7955b1a53e5fbd26deee8a417beb4f.js"
 );
 
 self.addEventListener('message', (event) => {
